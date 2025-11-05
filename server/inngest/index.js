25a7fcff68bacc6f02ec7c39a107f4b2af4a9480
devanshu-bisht23 <devanshubisht23@gmail.com>
2025-11-05 16:00:06 +0700

    change plural

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -14,7 +14,7 @@ const syncUserCreation = inngest.createFunction(
         await prisma.user.create({
             data:{
                 id: data.id,
-                email: data?.email_address[0]?.email_address,
+                email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             }
@@ -49,7 +49,7 @@ const syncUserUpdation = inngest.createFunction(
                 id: data.id
             },
             data:{
-                email: data?.email_address[0]?.email_address,
+                email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             }
